80f662798045be33248c4afe04bd9927a23ff1b5
John Boehr <jbboehr@gmail.com>
2013-04-12 18:27:05 -0700

    fixed paths for composerautoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,13 +3,13 @@
 $basepath = realpath(dirname(dirname(__FILE__)));
 $testpath = realpath(dirname(__FILE__));
 
-require $basepath . '/src/Exception.php';
-require $basepath . '/src/Expression.php';
-require $basepath . '/src/Query.php';
-require $basepath . '/src/ExtendedQuery.php';
-require $basepath . '/src/Delete.php';
-require $basepath . '/src/Insert.php';
-require $basepath . '/src/Select.php';
-require $basepath . '/src/Update.php';
+require $basepath . '/src/zsql/Exception.php';
+require $basepath . '/src/zsql/Expression.php';
+require $basepath . '/src/zsql/Query.php';
+require $basepath . '/src/zsql/ExtendedQuery.php';
+require $basepath . '/src/zsql/Delete.php';
+require $basepath . '/src/zsql/Insert.php';
+require $basepath . '/src/zsql/Select.php';
+require $basepath . '/src/zsql/Update.php';
 
 require $testpath . '/common.php';
