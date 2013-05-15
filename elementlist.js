e5140efa18e524780ee5761110a4724ed65ec2f8
root <root@mansairaku>
2013-05-14 18:24:48 -0700

    Initial commit

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Exception"],["c","zsql\\Delete"],["c","zsql\\Exception"],["c","zsql\\Expression"],["c","zsql\\ExtendedQuery"],["c","zsql\\Insert"],["c","zsql\\Query"],["c","zsql\\Select"],["c","zsql\\Update"]];
