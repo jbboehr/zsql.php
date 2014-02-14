f872e9fa83cf5518ceb89fc1a3e6dc17b732a307
John Boehr <jbboehr@gmail.com>
2014-02-14 12:12:01 -0800

    Updated docs to 0.2.0

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","zsql\\Delete"],["c","zsql\\Exception"],["c","zsql\\Expression"],["c","zsql\\ExtendedQuery"],["c","zsql\\Insert"],["c","zsql\\Query"],["c","zsql\\Select"],["c","zsql\\Update"]];
+ApiGen.elements = [["c","Exception"],["c","mysqli"],["c","mysqli_result"],["c","stdClass"],["c","zsql\\Database"],["c","zsql\\Delete"],["c","zsql\\Exception"],["c","zsql\\Expression"],["c","zsql\\ExtendedQuery"],["c","zsql\\Insert"],["c","zsql\\Model"],["c","zsql\\Query"],["c","zsql\\Result"],["c","zsql\\Select"],["c","zsql\\Update"]];
