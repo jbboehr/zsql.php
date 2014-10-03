6ea4c1b0ebebac040ad11984b6022bb1b9a6fe69
John Boehr <jbboehr@gmail.com>
2014-10-02 18:00:07 -0700

    Using PSR-4 autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,27 +1,4 @@
 <?php
 
-$basepath = realpath(dirname(dirname(__FILE__)));
-$testpath = realpath(dirname(__FILE__));
-
-if( empty($_SERVER['PHAR']) ) {
-  $srcpath = $basepath . '/src/zsql';
-} else {
-  require $basepath . '/build/zsql.phar';
-  $srcpath = 'phar://zsql.phar';
-}
-
-
-require $srcpath . '/Exception.php';
-require $srcpath . '/Expression.php';
-require $srcpath . '/Query.php';
-require $srcpath . '/ExtendedQuery.php';
-require $srcpath . '/Delete.php';
-require $srcpath . '/Insert.php';
-require $srcpath . '/Select.php';
-require $srcpath . '/Update.php';
-
-require $srcpath . '/Database.php';
-require $srcpath . '/Result.php';
-require $srcpath . '/Model.php';
-
-require $testpath . '/common.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/Common.php';
