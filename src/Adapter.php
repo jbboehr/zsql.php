129a301689f6e839226beaaa3cdc091461243ba7
John Boehr <jbboehr@gmail.com>
2016-10-20 00:48:56 -0700

    Move classes

---
+++ src/Adapter.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace zsql;
+
+use zsql\Expression;
+use zsql\QueryBuilder\Query;
+use zsql\QueryBuilder\Delete;
+use zsql\QueryBuilder\Insert;
+use zsql\QueryBuilder\Select;
+use zsql\QueryBuilder\Update;
+use zsql\Result\Result;
+
+interface Adapter
+{
+    /**
+     * Get affected rows
+     *
+     * @return integer
+     */
+    public function getAffectedRows();
+
+    /**
+     * Get the last insert ID
+     *
+     * @return integer
+     */
+    public function getInsertId();
+
+    /**
+     * Gets number of queries run using this adapter.
+     *
+     * @return integer
+     */
+    public function getQueryCount();
+
+    /**
+     * Executes an SQL query
+     *
+     * @param string|Query $query
+     * @return Result|integer|boolean
+     * @throws Exception
+     */
+    public function query($query);
+
+    /**
+     * Quote a value
+     *
+     * @param string|Expression $string
+     * @return string
+     */
+    public function quote($string);
+
+    /**
+     * Quote an identifier
+     *
+     * @param string $identifier
+     * @return string
+     */
+    public function quoteIdentifier($identifier);
+
+    /**
+     * Wrapper for Select
+     *
+     * @return Select
+     */
+    public function select();
+
+    /**
+     * Wrapper for Insert
+     *
+     * @return Insert
+     */
+    public function insert();
+
+    /**
+     * Wrapper for Update
+     *
+     * @return Update
+     */
+    public function update();
+
+    /**
+     * Wrapper for Delete
+     *
+     * @return Delete
+     */
+    public function delete();
+
+    /**
+     * @return boolean
+     */
+    public function ping();
+}
---
+++ src/Result.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace zsql;
+
+interface Result
+{
+    const FETCH_COLUMN = 0;
+    const FETCH_OBJECT = 1;
+    const FETCH_ASSOC = 2;
+    const FETCH_NUM = 3;
+
+    /**
+     * @param integer $mode
+     * @return array
+     */
+    public function fetchAll($mode = null);
+
+    /**
+     * @return string|integer|float|null
+     */
+    public function fetchColumn();
+
+    /**
+     * @param integer $mode
+     * @return array|object
+     */
+    public function fetchRow($mode = null);
+
+    /**
+     * Get result class
+     *
+     * @return string
+     */
+    public function getResultClass();
+
+    /**
+     * Set result class
+     *
+     * @param string $class
+     * @return $this
+     * @throws Exception
+     */
+    public function setResultClass($class);
+
+    /**
+     * @return integer
+     */
+    public function getResultMode();
+
+    /**
+     * @param integer $mode
+     * @return $this
+     * @throws Exception
+     */
+    public function setResultMode($mode);
+
+    /**
+     * Get result params
+     *
+     * @return array
+     */
+    public function getResultParams();
+
+    /**
+     * Set result params
+     *
+     * @param array $params
+     * @return $this
+     */
+    public function setResultParams(array $params = null);
+}
